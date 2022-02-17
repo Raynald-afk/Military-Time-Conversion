56ee99d632850782057ffda3a8061db46b4fe8d7
Raynald-afk <61276690+Raynald-afk@users.noreply.github.com>
2022-02-17 07:59:54 -0700

    MilitaryTimeConversion
    converts 12hr clock to military clock

--- Readme.md
+++ Readme.md
@@ -1,2 +1,4 @@
-## Random numbers
-IT generates password using the random and string module
+# Military time conversion
+
+The function converts 12 hour clock into a military time 
+with a string as the time parameter(s).
--- TimeConvection.py
+++ TimeConvection.py
@@ -1,8 +1,28 @@
-import random
-import string
 
-alph  = string.ascii_lowercase
-numbers  = [str(n) for n in range (10)]
-all = alph+ "".join(numbers)
-password = "".join(random.sample(all,10))
-print(f"New password:{password}")
+
+def Time(s):
+    """
+    This funtion takes in a string time as a parameter and then converts it to 
+    military time
+    """
+    if "PM" in s:
+        s = s.rstrip("PM").split(":")
+        if s[0] =="12":
+            return ":".join(s)
+        else:
+            time = int(s[0])+12
+            for i in s[1:]:
+                time = str(time) + ":" +i
+            return time
+
+
+    elif "AM" in s:
+        s = s.rstrip("AM").split(":")
+        if s[0] =="12":
+            time_h = "00"
+            for i in s[1:]:
+                time_h = time_h +":"+ i
+            return time_h
+        else:
+            return ":".join(s)
+        
